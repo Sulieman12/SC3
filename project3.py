a806152a9ec342abd6c15064b94f6fa035baddb4
Sulieman12 <99287919+Sulieman12@users.noreply.github.com>
2022-04-04 14:12:07 -0400

    Create project3.py

---
+++ project3.py
@@ -0,0 +1,14 @@
+
+import cmath
+
+a = float(input("Enter the value of a "))
+b = float(input("Enter the value of b "))
+c = float(input("Enter the value of c "))
+
+d = (b**2) - (4*a*c)
+
+
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print("your two soloutions are: ", sol1,sol2)
