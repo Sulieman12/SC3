f5e65dc32462849d1a35c73cc79765b249d8d799
Sulieman12 <99287919+Sulieman12@users.noreply.github.com>
2022-04-06 17:02:00 -0400

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,24 +1,22 @@
-#Sulieman Project #3
-#04-06-2022
+# Sulieman Project #3
+# 04-06-2022
 
-from math import sqrt
-
+from math import *
 
 NoRoot = True
 
-
 while NoRoot:
-  try:
-    a = float(input("Enter the value of a: \n"))
-    b = float(input("Enter the value of b: \n"))
-    c = float(input("Enter the value of c: \n"))
+    try:
+        a = float(input("Enter the value of a: \n"))
+        b = float(input("Enter the value of b: \n"))
+        c = float(input("Enter the value of c: \n"))
 
-    Value = (b**2)-(4*a*c)
-    s1 = (-b+sqrt(Value)/(2*a))
-    s2 = (-b-sqrt(Value)/(2*a))
+        Value = (b**2)-(4 * a * c)
+        s1 = (-b + sqrt(Value))/(2 * a)
+        s2 = (-b - sqrt(Value))/(2 * a)
 
-    print((s1,s2))
-    NoRoot = False 
+        print("Roots", (s1, s2))
+        NoRoot = False
 
-  except:
+    except:
         print("There Are no roots \n")
