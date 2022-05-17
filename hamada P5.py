2b29cf493c1e69a87ddbc1d31c5f925239be851d
Sulieman12 <99287919+Sulieman12@users.noreply.github.com>
2022-05-17 14:19:01 -0400

    Create hamada P5.py

---
+++ hamada P5.py
@@ -0,0 +1,17 @@
+def bye(filename):
+    f = open(filename, 'r')
+    char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,:/?! '
+    diction = {}
+    msg = ''
+
+    for line in f:
+        msg += line
+
+    for i in char:
+        diction[i] = 0
+
+    for i in msg:
+        diction[i] += 1
+
+    print(diction)
+    f.close
